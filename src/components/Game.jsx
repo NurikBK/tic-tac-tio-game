b94d762ae3b458867190c7ab6480c6c841fc1660
Nurzhan Bizhan <bknurik@gmail.com>
2022-11-06 19:19:20 +0600

    add logic to the game

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import { useState } from 'react';
 import Board from './Board';
+import { calculateWinner } from '../gameLogic';
 
 function Game() {
-  function onClick() {
-    
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [xIsNext, setXisNext] = useState(true);
+
+  const winner = calculateWinner(board);
+
+  function onClick(i) {
+    const boardCopy = [...board];
+    //if user click square is occupied or game is over, return
+    if (winner || board[i]) return;
+
+    // show X or O
+    boardCopy[i] = xIsNext ? 'X' : 'O';
+    setBoard(boardCopy);
+    setXisNext(!xIsNext);
   }
-  const squares = Array(9);
 
   function jumpTo() {}
 
   function renderMoves() {}
   return (
     <div>
-      <Board squares={squares} onClick={onClick} />
+      <Board squares={board} onClick={onClick} />
     </div>
   );
 }
