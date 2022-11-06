fe516d34f9b909d51b11dc7095916fcfb8f41c70
Nurzhan Bizhan <bknurik@gmail.com>
2022-11-06 18:22:11 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-
+import Board from './components/Board';
+import Game from './components/Game';
 function App() {
   return (
     <div className="App">
-   
+      <h1>Tic Tac Toe</h1>
+      <Game />
+      <Board />
     </div>
   );
 }
