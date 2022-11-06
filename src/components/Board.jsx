ccbdd910f49ec00d53d5123d4d4e98d2cf19ce93
Nurzhan Bizhan <bknurik@gmail.com>
2022-11-06 18:34:40 +0600

    Update Board.jsx

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,9 +1,11 @@
 import Square from './Square';
 
-function Board() {
-  function onClick() {}
-  
-  return <div></div>;
+function Board({ squares, onClick }) {
+  return (
+    <div className="board">
+      <Square value="1" onClick={onClick} />
+    </div>
+  );
 }
 
 export default Board;
