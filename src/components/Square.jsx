5441e19b81181395b1b086fc9744bf76997cb9f4
Nurzhan Bizhan <bknurik@gmail.com>
2022-11-06 18:50:05 +0600

    add style to the button

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
 function Square({ onClick, value }) {
-  return <button onClick={onClick}>{value}</button>;
+  const style = {
+    background: 'lightblue',
+    border: '2px solid darkblue',
+    fontSize: '30px',
+    fontWeight: '800',
+    cursor: 'pointer',
+    outline: 'none',
+  };
+
+  return (
+    <button style={style} onClick={onClick}>
+      {value}
+    </button>
+  );
 }
 
 export default Square;
