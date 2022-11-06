0d3e515ca666d06dbf67bd28ee98e67d286425da
Nurzhan Bizhan <bknurik@gmail.com>
2022-11-06 19:19:09 +0600

    styled board

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,9 +1,22 @@
 import Square from './Square';
 
 function Board({ squares, onClick }) {
+  const style = {
+    border: '4px solid darkblue',
+    borderRadius: '10px',
+    width: '250px',
+    height: '250px',
+    margin: '0 auto',
+    display: 'grid',
+    gridTemplate: 'repeat(3, 1fr) / repeat(3, 1fr)',
+  };
   return (
-    <div className="board">
-      <Square value="1" onClick={onClick} />
+    <div style={style}>
+      {squares.map((square, index) => {
+        return (
+          <Square key={index} value={square} onClick={() => onClick(index)} />
+        );
+      })}
     </div>
   );
 }
